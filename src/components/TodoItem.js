5b4e3330b4250dc7d3218d6ab8dd6cb9a9575efb
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 10:51:33 +0300

    Add checkbox to the todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 class TodoItem extends React.Component {
   render() {
-    return <li type="checkbox">{this.props.todo.title}</li>
+    return (
+      <li type="checkbox">
+        <input type="checkbox" checked={this.props.todo.completed} />
+        {this.props.todo.title}
+      </li>
+    )
   }
 }
 
