79ec4144277eb2d90e16e7c364961fced150082b
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 10:09:02 +0300

    Add checkbox type

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class TodoItem extends React.Component {
   render() {
-    return <li>{this.props.todo.title}</li>
+    return <li type="checkbox">{this.props.todo.title}</li>
   }
 }
 
